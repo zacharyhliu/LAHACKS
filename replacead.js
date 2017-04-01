770b8a2246931d7857be6f1b661fee07abaf6f2e
cindyz8735 <cindyz8735@gmail.com>
2017-04-01 15:34:22 -0700

    replace ad

--- replacead.js
+++ replacead.js
@@ -1,1 +1,20 @@
 //Replace Ad with anything - functions?
+// var ads = $(".ad").remove();
+$(".ad").each(function() {
+  // $(this).append("<div class='replaceAd' id='ra" + x + "'
+  //   ></div>");
+  // x++;
+  // var y = "#ra" + x;
+  // $(y).
+  var width = $(this).css( "width" );
+  var height = $(this).css("height");
+  console.log(width);
+  console.log(height);
+
+  $(this).after("<div style='height:'" + height + "; width:'" + width + "; display:block;>Test</div>")
+
+  // $(".replaceAd").width($(this).width());
+  $(this).remove();
+});
+
+
